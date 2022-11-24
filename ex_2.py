f9073a29adc42f7de1af5cd85f848a2734a8decc
Nika <nikarasevas@gmail.com>
2022-11-24 17:37:17 +0300

    done

--- ex_2.py
+++ ex_2.py
@@ -1,3 +1,51 @@
-# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех
+# Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех
 # значений предикат.
 
+
+X = False
+Y = False
+Z = False
+
+print((not(X or Y or Z)) == (not X and not Y and not Z))
+
+X = False
+Y = False
+Z = True
+
+print((not(X or Y or Z)) == (not X and not Y and not Z))
+
+X = False
+Y = True
+Z = False
+
+print((not(X or Y or Z)) == (not X and not Y and not Z))
+
+X = False
+Y = True
+Z = True
+
+print((not(X or Y or Z)) == (not X and not Y and not Z))
+
+X = True
+Y = False
+Z = False
+
+print((not(X or Y or Z)) == (not X and not Y and not Z))
+
+X = True
+Y = False
+Z = True
+
+print((not(X or Y or Z)) == (not X and not Y and not Z))
+
+X = True
+Y = True
+Z = False
+
+print((not(X or Y or Z)) == (not X and not Y and not Z))
+
+X = True
+Y = True
+Z = True
+
+print((not(X or Y or Z)) == (not X and not Y and not Z))
